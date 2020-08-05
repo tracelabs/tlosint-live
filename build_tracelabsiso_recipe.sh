a6d6837079288c266a17246002a839e299c24b5a
xFreed0m <13978578+xFreed0m@users.noreply.github.com>
2020-08-05 20:36:47 +0300

    Update build_tracelabsiso_recipe.sh

--- build_tracelabsiso_recipe.sh
+++ build_tracelabsiso_recipe.sh
@@ -98,6 +98,7 @@ function tlosint-install {
 		echo "[+] Kali ISO build process starting ... "
 		##### removing version check to allow build on ubuntu (DON'T REMOVE, NEED THIS FOR CI\CD)
 		sed -i '161s/.*/#exit 1/' /opt/live-build-config/build.sh
+		sed -i '166s/.*/#exit 1/' /opt/live-build-config/build.sh
 		sed -i '177s/.*/#exit 1/' /opt/live-build-config/build.sh
 		sed -i '182s/.*/#exit 1/' /opt/live-build-config/build.sh
 		$kali_path/build.sh --verbose --variant tracelabs -- --apt-http-proxy=${http_proxy}

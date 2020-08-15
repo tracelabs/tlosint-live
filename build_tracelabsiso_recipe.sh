5d192ec70e54d0c9702729ac271958fb376129d0
xFreed0m <13978578+xFreed0m@users.noreply.github.com>
2020-08-15 16:03:53 +0300

    changes
    change-log:
    
    install script divided to functions for easier changes and debugging in the future
    Testing if running as root when script starting
    Checking for internet access before starting the installation part, check will continue on fail (prevent false-positive)
    added sudo to the oneliner command
    removed version check from build.sh to allow build on OS other than kali - https://www.kali.org/tutorials/build-kali-with-live-build-on-debian-based-systems/ (currently used for the Github actions build)
    You can now build the ISO on Debian-based systems other than kali! (i.e ubuntu)
    Kali ISO creating is tested automatically on each push to master
    removing files downloaded
    reverting squid changes
    reveting kali sources add
    
    issues fixed:
    #10 - Script should create an option to create the respective config paths if they don't exist
    #35 - Squid proxy setting persists after ISO creation

--- build_tracelabsiso_recipe.sh
+++ build_tracelabsiso_recipe.sh
@@ -1,46 +1,150 @@
-#! /bin/bash
-# Abort the execution if any of the step fails
-set -e
+#!/bin/bash
+
 
 # Log output to STDOUT and to a file.
-logPath="squid_setup.log"
+export logPath="squid_setup.log"
 exec &> >( tee -a $logPath)
 
-# Clone the Kali live-build and Tracelabs repositories 
-git clone https://gitlab.com/kalilinux/build-scripts/live-build-config.git /opt/live-build-config
-git clone https://github.com/tracelabs/tlosint-live.git /opt/tlosint-live
+##### Fix display output for GUI programs (when connecting via SSH)
+export DISPLAY=:0.0
+export TERM=xterm
 
-kali_path="/opt/live-build-config"
-tl_path="/opt/tlosint-live"
 
-sudo -s -- <<EOF
-    if [ -d $kali_path ]
-    then
-        if [ -d $tl_path ]
-        then
-            apt-get update -y && apt-get upgrade -y && apt-get dist-upgrade -y
-            echo "Updates done ... "
+##### Check if we are running as root - else this script will fail
+function root_check {	
+	if [[ "${EUID}" -ne 0 ]]; then
+	  echo -e "[!] This script must be run as root" 1>&2
+	  echo -e "[!] Quitting..." 1>&2
+	  exit 1
+	else
+	  internet_access
+	fi
+}	
 
-            apt-get install curl git live-build cdebootstrap squid -y
-            echo "Live build pre-requisites installed ... "
+##### Check Internet access 
+function internet_access {
+	#--- Can we ping google?
+	for i in {1..10}; do ping -c 1 -W ${i} www.google.com &>/dev/null && break; done
+	#--- Run this, if we can't
+	if [[ "$?" -ne 0 ]]; then
+	  echo -e '[!]'" Possible DNS issues(?)" 1>&2
+	  echo -e '[!]'" Will try and use DHCP to 'fix' the issue" 1>&2
+	  chattr -i /etc/resolv.conf 2>/dev/null
+	  dhclient -r
+	  #--- Second interface causing issues?
+#	  ip addr show eth1 &>/dev/null
+#	  [[ "$?" == 0 ]] \
+#		&& route delete default gw 192.168.155.1 2>/dev/null
+	  #--- Request a new IP
+	  dhclient
+	  dhclient eth0 2>/dev/null
+	  dhclient wlan0 2>/dev/null
+	  #--- Wait and see what happens
+	  sleep 15s
+	  _TMP="true"
+	  _CMD="$(ping -c 1 8.8.8.8 &>/dev/null)"
+	  if [[ "$?" -ne 0 && "$_TMP" == "true" ]]; then
+		_TMP="false"
+		echo -e '[!]'" No Internet access" 1>&2
+		echo -e '[!]'" You will need to manually fix the issue, before re-running this script" 1>&2
+	  fi
+	  _CMD="$(ping -c 1 www.google.com &>/dev/null)"
+	  if [[ "$?" -ne 0 && "$_TMP" == "true" ]]; then
+		_TMP="false"
+		echo -e '[!]'" Possible DNS issues(?)" 1>&2
+		echo -e '[!]'" You will need to manually fix the issue, before re-running this script" 1>&2
+	  fi
+	  if [[ "$_TMP" == "false" ]]; then
+		(dmidecode | grep -iq virtual) && echo -e " [i] VM Detected"
+		(dmidecode | grep -iq virtual) && echo -e " [i] Try switching network adapter mode (e.g. NAT/Bridged)"
+		echo -e '[!]'" You will need to manually fix the issue, before re-running this script, trying anyway" 1>&2
+		tlosint-install
+	  fi
+	else
+	  echo -e " [i] Detected Internet access" 1>&2
+	  tlosint-install
+	fi
+}
 
-            wget -O /etc/squid/squid.conf https://raw.githubusercontent.com/prateepb/kali-live-build/master/squid.conf
-            /etc/init.d/squid start
-            grep -qxF "http_proxy=http://localhost:3128/"  /etc/environment || echo "http_proxy=http://localhost:3128/" >> /etc/environment
-            echo "Squid set-up completed .... "
+##### tlosint-live installation
+function tlosint-install {
+	
+	##### OS Version
+	OS_VERSION=$(cat /etc/issue)
+
+	
+	##### Disabling the lockscreen
+	#xset s 0 0
+    #xset s off
+    #gsettings set org.gnome.desktop.session idle-delay 0
+	
+	kali_path="/opt/live-build-config"
+	tl_path="/opt/tlosint-live"
+	
+	if [ -d "$kali_path" ]; then
+		
+	  if [ -d "$tl_path" ]; then
+	    
+		if [ "$OS_VERSION" != "Kali GNU/Linux Rolling \n \l" ]; then
+		  apt-get -qq install gnupg
+  		  wget -q 'https://archive.kali.org/archive-key.asc'
+		  apt-key add archive-key.asc
+		  cat /etc/apt/sources.list > /etc/apt/sources.list.orig
+		  echo "deb http://http.kali.org/kali kali-rolling main non-free contrib" >> /etc/apt/sources.list
+		  
+		  wget https://http.kali.org/pool/main/k/kali-archive-keyring/kali-archive-keyring_2020.2_all.deb
+		  apt-get update -qq
+		  dpkg --configure -a
+		  dpkg -i kali-archive-keyring_2020.2_all.deb
+
+	    fi
+		
+		apt-get update -qq -y 
+		dpkg --configure -a
+		apt --fix-broken install
+		#apt-get upgrade -y
+		#apt-get dist-upgrade -y
+		echo "[+] Updates done ... "
+
+		apt-get install curl git -y
+		apt-get install live-build -y
+		apt-get install cdebootstrap -y
+		apt-get install squid -y
+		echo "[+] Live build pre-requisites installed ... "
+
+		wget -O /etc/squid/squid.conf https://raw.githubusercontent.com/prateepb/kali-live-build/master/squid.conf
+		/etc/init.d/squid start
+		grep -qxF "http_proxy=http://localhost:3128/"  /etc/environment || echo "http_proxy=http://localhost:3128/" >> /etc/environment
+		echo "[+] Squid set-up completed .... "
 
 	    # Copy all the files required for the Tracelabs ISO to the latest Kali live-build repo
-            cp -rv $tl_path/kali-config/variant-tracelabs/ $kali_path/kali-config/
-            cp -rv $tl_path/kali-config/common/hooks/normal $kali_path/kali-config/common/hooks/
-            cp -rv $tl_path/kali-config/common/includes.chroot/etc/* $kali_path/kali-config/common/includes.chroot/etc/
-            cp -rv $tl_path/kali-config/common/includes.chroot/usr/* $kali_path/kali-config/common/includes.chroot/usr/
+		cp -rfv $tl_path/kali-config/variant-tracelabs/ $kali_path/kali-config/
+		cp -rfv $tl_path/kali-config/common/hooks/normal $kali_path/kali-config/common/hooks/
+		cp -rfv $tl_path/kali-config/common/includes.chroot/etc/* $kali_path/kali-config/common/includes.chroot/etc/
+		cp -rfv $tl_path/kali-config/common/includes.chroot/usr/* $kali_path/kali-config/common/includes.chroot/usr/
 
-            echo "Kali ISO build process starting ... "
-            $kali_path/build.sh --verbose --variant tracelabs -- --apt-http-proxy=${http_proxy}
-        else
-            echo "Tracelabs path does not exist"
-        fi
-    else
-        echo "Kali live build path does not exist"
-    fi
-EOF
+		echo "[+] Kali ISO build process starting ... "
+		##### removing version check to allow build on ubuntu (DON'T REMOVE, NEED THIS FOR CI\CD)
+		sed -i '161s/.*/#exit 1/' /opt/live-build-config/build.sh
+		sed -i '166s/.*/#exit 1/' /opt/live-build-config/build.sh
+		sed -i '177s/.*/#exit 1/' /opt/live-build-config/build.sh
+		sed -i '182s/.*/#exit 1/' /opt/live-build-config/build.sh
+		$kali_path/build.sh --verbose --variant tracelabs -- --apt-http-proxy=${http_proxy}
+		
+		##### Cleanup
+		apt-get remove -qq squid -y
+		rm -f /etc/squid/squid.conf
+		rm -f kali-archive-keyring_2020.2_all.deb
+		cat /etc/apt/sources.list.orig > /etc/apt/sources.list 
+	  fi
+
+	else
+		# Clone the Kali live-build and Tracelabs repositories 
+		echo "[+] tlosint-live & live-build-config directories not found, creating."
+		git clone https://gitlab.com/kalilinux/build-scripts/live-build-config.git /opt/live-build-config
+		git clone https://github.com/tracelabs/tlosint-live.git /opt/tlosint-live
+		tlosint-install
+	fi
+}
+		
+root_check

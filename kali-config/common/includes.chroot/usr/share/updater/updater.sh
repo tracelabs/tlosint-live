6eba1909d7bc07c8fb231e90f421c7cac7e136cd
xFreed0m <13978578+xFreed0m@users.noreply.github.com>
2020-10-16 16:07:18 +0300

    Update updater.sh

--- kali-config/common/includes.chroot/usr/share/updater/updater.sh
+++ kali-config/common/includes.chroot/usr/share/updater/updater.sh
@@ -26,8 +26,12 @@ tput setaf 2;echo "[+] Done."
 
 tput setaf 5;echo "[+] Updating LittleBrother..."
 {
-        cd /usr/share/LittleBrother
-        sudo git pull https://github.com/Lulz3xploit/LittleBrother
+	if [ -d "/usr/share/LittleBrother" ]; then        
+	
+		cd /usr/share/LittleBrother
+        	sudo git pull https://github.com/Lulz3xploit/LittleBrother --rebase
+	else
+		sudo git clone https://github.com/Lulz3xploit/LittleBrother /usr/share/LittleBrother
 } 
 tput setaf 2;echo "[+] Done."
 
@@ -35,17 +39,26 @@ tput setaf 2;echo "[+] Done."
 
 tput setaf 5;echo "[+] Updating PhoneInfoga..."
 {
-        cd /usr/share/PhoneInfoga
-        sudo git pull https://github.com/sundowndev/PhoneInfoga.git
+        if [ -d "/usr/share/phoneinfoga" ]; then        
+	
+		cd /usr/share/phoneinfoga
+        	sudo git pull https://github.com/sundowndev/PhoneInfoga.git --rebase
+	else
+		sudo git clone https://github.com/sundowndev/PhoneInfoga.git /usr/share/phoneinfoga
 } 
+
 tput setaf 2;echo "[+] Done."
 
 ###################
 
 tput setaf 5;echo "[+] Updating theHarvester..."
 {
-        cd /usr/share/theHarvester
-        sudo git pull https://github.com/laramies/theHarvester.git
+        if [ -d "/usr/share/theHarvester" ]; then        
+	
+		cd /usr/share/theHarvester
+        	sudo git pull https://github.com/laramies/theHarvester.git  --rebase
+	else
+		sudo git clone https://github.com/laramies/theHarvester.git /usr/share/theHarvester
 } 
 tput setaf 2;echo "[+] Done."
 
@@ -53,8 +66,12 @@ tput setaf 2;echo "[+] Done."
 
 tput setaf 5;echo "[+] Updating ExifScan..."
 {
-        cd /usr/share/exifscan
-        sudo git pull https://github.com/rcook/exifscan.git
+        if [ -d "/usr/share/exifscan" ]; then        
+	
+		cd /usr/share/exifscan
+        	sudo git pull https://github.com/rcook/exifscan.git --rebase
+	else
+		sudo git clone https://github.com/rcook/exifscan /usr/share/exifscan
 } 
 tput setaf 2;echo "[+] Done."
 
@@ -62,8 +79,12 @@ tput setaf 2;echo "[+] Done."
 
 tput setaf 5;echo "[+] Updating DumpsterDiver..."
 {
-        cd /usr/share/DumpsterDiver
-        sudo git pull https://github.com/securing/DumpsterDiver.git
+        if [ -d "/usr/share/DumpsterDiver" ]; then        
+	
+		cd /usr/share/DumpsterDiver
+		sudo git pull https://github.com/securing/DumpsterDiver.git --rebase
+	else
+		sudo git clone https://github.com/securing/DumpsterDiver /usr/share/DumpsterDiver
 } 
 tput setaf 2;echo "[+] Done."
 
@@ -71,8 +92,12 @@ tput setaf 2;echo "[+] Done."
 
 tput setaf 5;echo "[+] Updating Sherlock..."
 {
-        cd /usr/share/sherlock
-        sudo git pull https://github.com/sherlock-project/sherlock.git
+        if [ -d "/usr/share/sherlock" ]; then        
+	
+		cd /usr/share/sherlock
+        	sudo git pull https://github.com/sherlock-project/sherlock.git --rebase
+	else
+		sudo git clone https://github.com/sherlock-project/sherlock /usr/share/sherlock
 } 
 tput setaf 2;echo "[+] Done."
 
@@ -81,8 +106,12 @@ tput setaf 2;echo "[+] Done."
 
 tput setaf 5;echo "[+] Updating Infoga..."
 {
-        cd /usr/share/Infoga
-        sudo git pull https://github.com/m4ll0k/Infoga.git
+        if [ -d "/usr/share/Infoga" ]; then        
+	
+		cd /usr/share/Infoga
+        	sudo git pull https://github.com/m4ll0k/Infoga.git --rebase
+	else
+		sudo git clone https://github.com/m4ll0k/Infoga /usr/share/Infoga
 } 
 tput setaf 2;echo "[+] Done."
 
@@ -91,8 +120,12 @@ tput setaf 2;echo "[+] Done."
 tput setaf 5;echo "[+] Updating Metagoofil..."
 {
 
-	cd /usr/share/metagoofil
-        sudo git pull https://github.com/opsdisk/metagoofil.git
+	if [ -d "/usr/share/metagoofil" ]; then        
+	
+		cd /usr/share/metagoofil
+        	sudo git pull https://github.com/opsdisk/metagoofil.git --rebase
+	else
+		sudo git clone https://github.com/opsdisk/metagoofil /usr/share/metagoofil
 } 
 tput setaf 2;echo "[+] Done."
 
@@ -100,10 +133,14 @@ tput setaf 2;echo "[+] Done."
 
 tput setaf 5;echo "[+] Updating OSINT-Search..."
 {
-        cd /usr/share/OSINT-Search
-        sudo git pull https://github.com/am0nt31r0/OSINT-Search.git
-        sudo pip3 install git+https://github.com/abenassi/Google-Search-API --upgrade
-        sudo pip3 install https://github.com/PaulSec/API-dnsdumpster.com/archive/master.zip --user
+        if [ -d "/usr/share/OSINT-Search" ]; then        
+	
+		cd /usr/share/OSINT-Search
+        	sudo git pull https://github.com/am0nt31r0/OSINT-Search.git --rebase
+        	sudo pip3 install git+https://github.com/abenassi/Google-Search-API --upgrade
+       		sudo pip3 install https://github.com/PaulSec/API-dnsdumpster.com/archive/master.zip --user
+	else
+		sudo git clone https://github.com/am0nt31r0/OSINT-Search /usr/share/OSINT-Search
 } 
 tput setaf 2;echo "[+] Done."
 
@@ -111,8 +148,12 @@ tput setaf 2;echo "[+] Done."
 
 tput setaf 5;echo "[+] Updating Stego Toolkit..."
 {
-        cd /usr/share/stego-toolkit
-        sudo git pull https://github.com/DominicBreuker/stego-toolkit.git
+        if [ -d "/usr/share/stego-toolkit" ]; then        
+	
+		cd /usr/share/stego-toolkit
+        	sudo git pull https://github.com/DominicBreuker/stego-toolkit.git --rebase
+	else
+		sudo git clone https://github.com/DominicBreuker/stego-toolkit.git  /usr/share/stego-toolkit
 } 
 tput setaf 2;echo "[+] Done."
 
@@ -120,16 +161,15 @@ tput setaf 2;echo "[+] Done."
 
 
 tput setaf 5;echo "[+] Updating sn0int..."
-# RS - The instructions below are for CentOS install, not debian - https://sn0int.readthedocs.io/en/latest/install.html#debian-ubuntu-kali
-#{
-#        cd /usr/share/sn0int
-#        sudo git pull https://github.com/kpcyrd/sn0int.git
-#        cargo install -f --path .
-#} 
 {
-        sudo apt upgrade -y sn0int
-
-} 
+        if [ -d "/usr/share/sn0int" ]; then        
+	
+		cd /usr/share/sn0int
+        	sudo git pull https://github.com/kpcyrd/sn0int.git --rebase
+        	cargo install -f --path .
+	else
+		sudo git clone https://github.com/kpcyrd/sn0int /usr/share/sn0int
+}  
 tput setaf 2;echo "[+] Done."
 
 
@@ -137,9 +177,13 @@ tput setaf 2;echo "[+] Done."
 
 tput setaf 5;echo "[+] Updating Spiderpig..."
 {
-        cd /usr/share/Spiderpig
-        sudo git pull https://github.com/hatlord/Spiderpig.git
-        bundle install
+        if [ -d "/usr/share/Spiderpig" ]; then        
+	
+		cd /usr/share/Spiderpig
+        	sudo git pull https://github.com/hatlord/Spiderpig.git --rebase
+        	bundle install
+	else
+		sudo git clone https://github.com/hatlord/Spiderpig /usr/share/Spiderpig
 }
 tput setaf 2;echo "[+] Done."
 
@@ -147,8 +191,12 @@ tput setaf 2;echo "[+] Done."
 
 tput setaf 5;echo "[+] Updating WhatsMyName..."
 {
-        cd /usr/share/WhatsMyName
-        sudo git pull https://github.com/WebBreacher/WhatsMyName.git
+        if [ -d "/usr/share/WhatsMyName" ]; then        
+	
+		cd /usr/share/WhatsMyName
+        	sudo git pull https://github.com/WebBreacher/WhatsMyName.git --rebase
+	else
+		sudo git clone https://github.com/WebBreacher/WhatsMyName /usr/share/WhatsMyName
 }
 tput setaf 2;echo "[+] Done."
 
@@ -156,7 +204,11 @@ tput setaf 2;echo "[+] Done."
 
 tput setaf 5;echo "[+] Updating WikiLeaker..."
 {
-        cd /usr/share/WikiLeaker
-        sudo git pull https://github.com/jocephus/WikiLeaker.git
+        if [ -d "/usr/share/WikiLeaker" ]; then        
+	
+		cd /usr/share/WikiLeaker
+        	sudo git pull https://github.com/jocephus/WikiLeaker.git --rebase
+	else
+		sudo git clone https://github.com/jocephus/WikiLeaker /usr/share/WikiLeaker
 }
 tput setaf 2;echo "[+] Done."

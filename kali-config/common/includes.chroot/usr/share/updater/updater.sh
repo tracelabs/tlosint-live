ae82f0bcc1ee16881870c944489e813a0b78500b
5nacks <23207476+5nacks@users.noreply.github.com>
2021-03-14 22:20:17 +1030

    Prepare update.sh for master
    Prepare update.sh for push to master

--- kali-config/common/includes.chroot/usr/share/updater/updater.sh
+++ kali-config/common/includes.chroot/usr/share/updater/updater.sh
@@ -2,13 +2,13 @@
 
 cd /home/osint
 
-wget -O updater-current.sh https://raw.githubusercontent.com/tracelabs/tlosint-live/dev/updater-current.sh
+wget -O updater-current.sh https://raw.githubusercontent.com/tracelabs/tlosint-live/master/updater-current.sh
 
 chmod +x updater-current.sh
 
 sudo /home/osint/updater-current.sh
 
-wget -O bookmarks.html https://raw.githubusercontent.com/tracelabs/tlosint-live/dev/bookmarks.html
+wget -O bookmarks.html https://raw.githubusercontent.com/tracelabs/tlosint-live/master/bookmarks.html
 
 rm -f updater-current.sh
 
